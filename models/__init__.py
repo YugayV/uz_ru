7e9af2b44db0adc28782295e030b877f66fbf79e
Yugay_Vitaliy <vamp.09.94@gmail.com>
2026-01-05 15:35:08 +0900

    1.13.1

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.database import Base
-from app.models.user import User
-from app.models.level import Level
-from app.models.lesson import Lesson
-
-from app.models.progress import UserLessonProgress
-from app.models.ai_usage import AIUsage
