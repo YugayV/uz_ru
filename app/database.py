731b032ed8412cc065d090b870b486d0d373cdd4
Yugay_Vitaliy <vamp.09.94@gmail.com>
2026-01-11 19:48:53 +0900

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,10 +6,17 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app/db") 
 
-engine = create_engine( 
-    DATABASE_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True
-
-)
+# Conditionally create the engine based on the database type
+if DATABASE_URL.startswith("sqlite"):
+    # Use check_same_thread only for SQLite
+    engine = create_engine( 
+        DATABASE_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True
+    )
+else:
+    # Do not use check_same_thread for PostgreSQL or other DBs
+    engine = create_engine(
+        DATABASE_URL, pool_pre_ping=True
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
